4456c27294626e49e236fd410f05e7df88338815
HP <HP@LAPTOP-MNAV15D0>
2024-04-26 15:42:58 +0800

    Practica6

---
+++ src/tarea6/Tarea6.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tarea6;
+
+/**
+ *
+ * @author HP
+ */
+public class Tarea6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Tarea6EDClase miCarrito = new Tarea6EDClase();
+
+        miCarrito.Descuento(100, 5);
+    }
+    
+}
